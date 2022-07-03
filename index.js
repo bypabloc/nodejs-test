15a73a41bd44ad848928ce9052b95dd542b0e6c0
Pablo <pacg1991@gmail.com>
2022-07-03 17:17:53 -0500

    hackerrank -> Save the Prisoner!
    https://www.hackerrank.com/challenges/save-the-prisoner/problem

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function main(){
-    const params = arguments;
-    console.log('params: ', params);
-    return 'Hello World';
+function main(n, m, s){
+    return ((m - 1) + (s - 1)) % n + 1;
 }
 module.exports = main;
--- test.js
+++ test.js
@@ -1,5 +1,20 @@
 const main = require('./index.js');
 
-test('main', () => {
-    expect(main('Hello World')).toBe('Hello Worlda');
-});
+const tests = [
+    {
+        'params': [4, 15, 2],
+        'expected': 4,
+    },
+    {
+        'params': [5, 2, 1],
+        'expected': 2,
+    },
+    {
+        'params': [5, 2, 2],
+        'expected': 3,
+    },
+]
+
+test.each(tests)('main', ({params, expected}) => {
+    expect(main(...params)).toBe(expected);
+})
